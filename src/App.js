899a07c2c84057be4e4d00c122a994a77ba2c4c1
unknown <you@example.com>
2024-01-09 23:22:39 +0900

    todo 앱 시작

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// react 라이브러리에서 Component를 가져오는 것
+import React, {Component} from "react";
+// css import 하기!
+import "./App.css";
+// 제공되는 Component를 사용하기 위해 extends(확장)해주는 것
+export default class App extends Component {
+  render() {
+    return(
+      <div className="container">
+        <div className="todoBlock">
+<div className="title">
+  <h1>할 일 목록</h1>
+
+</div>
+
+        </div>
+      
+      </div>
+    )
+  }
 }
-
-export default App;
